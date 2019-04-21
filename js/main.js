6206b72348486c5f92a05d0bbf6128e34abbf5b6
Nicolai <iplexus@icloud.com>
2019-04-22 02:22:55 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,26 +1,33 @@
-const animateHeader = function(Header, ratio) {
-    if (ratio > 0) {
-        if (false && Header.tagName === "H2") {
-            Header.classList.add("in-view");
+{
+    const animateHeader = function(Header, ratio) {
+        if (ratio > 0) {
+            if (false && Header.tagName === "H2") {
+                Header.classList.add("in-view");
+            } else {
+                Header.classList.add("in-view");
+            }
         } else {
-            Header.classList.add("in-view");
+            Header.classList.remove("in-view");
         }
-    } else {
-        Header.classList.remove("in-view");
+    };
+
+    const Headers = document.querySelectorAll("h2");
+    const HeadersObserver = new IntersectionObserver(
+        entries => {
+            return entries.forEach(e => {
+                animateHeader(e.target, e.intersectionRatio);
+            });
+        },
+        { threshold: 1.0 }
+    );
+    for (const Header of Headers) {
+        HeadersObserver.observe(Header);
+        HeadersObserver.observe(Header.closest("article"));
     }
-};
 
-const Headers = document.querySelectorAll("h2");
-const HeadersObserver = new IntersectionObserver(
-    entries => {
-        return entries.forEach(e => {
-            animateHeader(e.target, e.intersectionRatio);
-        });
-    },
-    { threshold: 1.0 }
-);
-for (const Header of Headers) {
-    HeadersObserver.observe(Header);
-    HeadersObserver.observe(Header.closest("article"));
+    imagesLoaded(
+        document.querySelectorAll(".grid__item-img"),
+        { background: true },
+        () => document.body.classList.remove("loading")
+    );
 }
-
