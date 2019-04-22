6e24ef25fc357a16f0b8e204695f80ebd0277bfe
Nicolai <iplexus@icloud.com>
2019-04-22 04:41:04 +0300

    update loader

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,7 @@
 $(window).on("load", function() {
     setTimeout(function() {
         $(".page__loader").fadeOut("slow", function() {
-            $(".body").removeClass("loading");
+            $("body").removeClass("loading");
         });
     }, 3500);
 });
